3936272e11667b1f8939b5cc20579348dfeb3edc
harikanani <hkanani191@gmail.com>
2020-10-16 19:48:49 +0530

    adding website;'

---
+++ account.html
@@ -0,0 +1,3 @@
+<?php
+include 'new2.php'
+?>
---
+++ new 2.php
@@ -0,0 +1,69 @@
+<?php
+?>
+<html>
+<body>
+<div class="header">
+	
+		<div class="container-width">
+			<div class="header-top">
+				<div class="header-left">
+					<div class="language-outer">
+						<a href="#" class="headtext-language">English</a>
+						<div class="language-hover">
+							<ul>
+							<li> <a href="#"> English </a></li>
+							<li> <a href="#"> French </a></li>
+							<li> <a href="#"> English </a></li>
+							</ul>
+					    </div>
+					</div>
+						<div class="currency-outer">
+								<a href="#" class="headtext-currency">Inr</a>
+							<div class="currency-hover">
+								<ul>
+									<li> <a href="#"> Inr </a></li>
+									<li> <a href="#"> French </a></li>
+									<li> <a href="#"> Usd </a></li>
+								</ul>
+							</div>
+						</div>
+				</div>
+			
+			<div class="header-right">
+				<div class="like">
+					<a href="#"></a>
+				</div>
+				<div class="my-account">
+					<a href="#"></a>
+				</div>
+					<div class="search-outer">
+						<div class="search-inner">
+							<form action="#" >
+								<input type="text" class="search-input" placeholder="search your catalog"/>
+								<input type="submit" class="search-button" value="search">
+							</form>
+						</div>
+					</div>
+			</div>
+		</div>
+		</div>
+	
+		<div class="container-width">
+			<div class="header-bottom">
+				<div class="header-logo">
+					<a href="#"><img class="headlogo" src="gymlogo.jpg"/></a>
+				</div>
+				<div class="header-cart">
+					<div class="header-cart-inner">
+						<div class="cart-image"></div>
+							<div class="cart-content">
+								<p>Shopping cart</p>
+								<p>0.00</p>
+							</div>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
+</html>
+</body>
